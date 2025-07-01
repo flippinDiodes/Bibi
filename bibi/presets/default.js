bca986d82c71ecf37ff6f056de176170af6eb3f5
flippinDiodes <144732985+flippinDiodes@users.noreply.github.com>
2025-07-01 17:13:48 +0100

    Update default.js

--- bibi/presets/default.js
+++ bibi/presets/default.js
@@ -60,10 +60,10 @@ Bibi.preset({
 
 "ui-font-family"             : "", // CSS font-family value as "'Helvetica', sans-serif" or ""
 
-"item-padding-left"          : 12, // Number of pixel (It affects only for reflowable books.)
-"item-padding-right"         : 12, // Number of pixel (It affects only for reflowable books.)
-"item-padding-top"           : 12, // Number of pixel (It affects only for reflowable books.)
-"item-padding-bottom"        : 12, // Number of pixel (It affects only for reflowable books.)
+"item-padding-left"          : 36, // Number of pixel (It affects only for reflowable books.)
+"item-padding-right"         : 36, // Number of pixel (It affects only for reflowable books.)
+"item-padding-top"           : 36, // Number of pixel (It affects only for reflowable books.)
+"item-padding-bottom"        : 36, // Number of pixel (It affects only for reflowable books.)
 
 "spread-gap"                 : 0, // Number of pixel (It affects only in paged view mode.)
 "spread-margin"              : 0, // Number of pixel
